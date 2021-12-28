31a2dea3152b3967b9856d16eb32351a32664cce
gureumwoon <vfrvfr3221@gmail.com>
2021-12-28 19:09:25 +0900

    navbar scroll & click 이동

---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar-dark');
+    } else {
+        navbar.classList.remove('navbar-dark');
+    }
+});
+
+const navbarMenu = document.querySelector('.navbar-menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link === null) {
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+})
