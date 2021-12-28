d0cc243c339226f38636949e75cca9fa122c09bb
gureumwoon <vfrvfr3221@gmail.com>
2021-12-28 21:16:57 +0900

    JS contact btn

--- main.js
+++ main.js
@@ -17,6 +17,16 @@ navbarMenu.addEventListener('click', (event) => {
     if (link === null) {
         return;
     }
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({ behavior: 'smooth' });
+    scrollIntoView(link);
 })
+
+const contactBtn = document.querySelector('.home-contact')
+contactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
+
